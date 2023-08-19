a259e32ffc5158f536b8e73cd6d99514ee681faa
thalita <e107ti@etec.sp.gov.br>
2023-08-18 21:50:25 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Lava, escorre, agita");
+Console.WriteLine("a areia. E enfim, na batéia,");
+Console.WriteLine("fica uma pepita.");
+
